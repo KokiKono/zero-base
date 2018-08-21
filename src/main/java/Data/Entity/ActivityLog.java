62842e6f509f72d80c09d76da4c3497e11099aad
kokikono <kk.k.0421@outlook.jp>
2018-08-21 21:43:53 +0900

    Create

--- src/main/java/Data/Entity/ActivityLog.java
+++ src/main/java/Data/Entity/ActivityLog.java
@@ -1,4 +1,29 @@
 package Data.Entity;
 
+import java.util.Date;
+
 public class ActivityLog {
+
+    private int id;
+    private String json;
+    private Date createAt;
+
+    public ActivityLog(int id, String json, Date createAt) {
+        this.id = id;
+        this.json = json;
+        this.createAt = createAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+
+    public Date getCreateAt() {
+        return createAt;
+    }
 }
