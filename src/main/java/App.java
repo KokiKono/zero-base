361dfefe46acb4ddeb43c540e32d3dd4cb9fe9d8
kokikono <kk.k.0421@outlook.jp>
2018-08-21 21:50:28 +0900

    Add:　APIからログを取得し、DBに保存するまで

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,34 @@
+import Data.DataStore.Activity.ActivityDataStore;
+import Data.DataStore.Activity.ActivityDataStoreImpl;
+import Data.DataStore.Authorize.ActivityAuthorizeDataStoreImpl;
+import Data.DataStore.Authorize.AuthorizeDataStore;
+import Domain.Repository.Activity.ActivityRepository;
+import Domain.Repository.Activity.ActivityRepositoryImpl;
+import Domain.Repository.Activity.AuthorizeRepository;
+import Domain.Repository.Activity.AuthorizeRepositoryImpl;
+import Domain.UseCase.ActivityUseCase;
+import Domain.UseCase.ActivityUseCaseImpl;
+import com.google.api.services.appsactivity.Appsactivity;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            AuthorizeDataStore authorizeDataStore
+                    = new ActivityAuthorizeDataStoreImpl();
+            AuthorizeRepository<Appsactivity> authorizeRepository
+                    = new AuthorizeRepositoryImpl(authorizeDataStore);
+
+            ActivityDataStore activityDataStore
+                    = new ActivityDataStoreImpl(authorizeRepository.getService());
+            ActivityRepository activityRepository
+                    = new ActivityRepositoryImpl(activityDataStore);
+
+            ActivityUseCase activityUseCase
+                    = new ActivityUseCaseImpl(authorizeRepository, activityRepository);
+
+            activityUseCase.loadActivityLog();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
